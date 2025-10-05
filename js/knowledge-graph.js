3ad16423a68ae4b8dc60f8d4f824bc2a759d40ba
wangyiyang066-cyber <wangyiyang066@gmail.com>
2025-10-05 19:29:19 +0800

    Update knowledge-graph.js

--- js/knowledge-graph.js
+++ js/knowledge-graph.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // --- 2. 配置与初始化 ---
-    const SUPABASE_URL = '你自己的 Supabase Project URL'; // <<< 填入你的信息
-    const SUPABASE_ANON_KEY = '你自己的 Supabase anon public 密钥'; // <<< 填入你的信息
+    const SUPABASE_URL = 'https://ghuyiwhqdellucjxqiwj.supabase.co'; // <<< 填入你的信息
+    const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdodXlpd2hxZGVsbHVjanhxaXdqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NzQzNDA5NCwiZXhwIjoyMDczMDEwMDk0fQ.op6RPiEDsjSnwy5yMRq3Got0dfLzPxGKWc0PFa8D5Go'; // <<< 填入你的信息
     const { createClient } = supabase;
     const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
     const editor = new Drawflow(document.getElementById("drawflow"));
